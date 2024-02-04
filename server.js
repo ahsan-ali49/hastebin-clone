321dd953fd4b1287ab8ac402a67f481a24c8dffc
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-04 13:17:15 +0500

    frontend setup

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>{
     const code = `Welcome to Wastebin!
-    
+
 Use the commands in the top right corner
 to create a new file to share with others.`
-    res.render("code-display", {code: code})
+    res.render("code-display", {code, lineNumbers: code.split("\n".length)})
 });
 
 
