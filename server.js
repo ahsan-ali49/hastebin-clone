6dafd422c50058d628fcff6da3695d6415405300
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-04 13:36:51 +0500

    setup css-styles

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
-    const code = `Welcome to Wastebin!
+const code = `Welcome to Wastebin!
 
 Use the commands in the top right corner
 to create a new file to share with others.`
-    res.render("code-display", {code, lineNumbers: code.split("\n".length)})
+    res.render("code-display", {code})
 });
 
 
